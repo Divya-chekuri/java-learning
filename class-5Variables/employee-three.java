46b5746bfb4a84d569b83a6eb8ee3cfa809b55b9
Divya-chekuri <divyachekuri00@gmail.com>
2024-08-30 13:44:00 +0530

    tst

--- class-5Variables/employee-three.java
+++ class-5Variables/employee-three.java
@@ -1,15 +1,15 @@
 class Employee {
-    int eid = 101;
+    int eid = 10;
 
     public void m1() {
-        System.out.println("Employee m1()");
+        System.out.println("Employee class - m1 method");
 
     }
 
     public static void main(String[] args) {
         // non-static members-using object
-        Employee e1 = new Employee();
+        Employee e1=new Employee()
         System.out.println(e1.eid);
-        e1.m1();
+         e1.m1();
     }
 }
